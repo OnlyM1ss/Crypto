4d080f58394e94cd2adf6259a68905963a10a131
OnlyM1ss <45197509+OnlyM1ss@users.noreply.github.com>
2020-01-07 22:55:04 +0200

    Add files via upload

--- Caesar.py
+++ Caesar.py
@@ -1,5 +1,10 @@
+import string
+
 uper_list = ['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
 low_list = ['А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+string_upercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+string_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 class Caesar: 
 	def encryptCaesar(msg,shift): 
 		ret = '' 
@@ -25,3 +30,27 @@ class Caesar:
 			else:
 				ret+=x
 		return ret
+	def encrypteng(msg,shift):
+		ret = ''
+		for x in msg:
+			if x in string_upercase:
+				ind = string_upercase.index(x)
+				ret += string_upercase[ind + shift]
+			if x in string_lowercase:
+				ind = string_lowercase.index(x)
+				ret += string_lowercase[ind + shift]
+			else:
+				ret +=x
+			return ret
+	def decrypteng(msg,shift):
+		ret = ''
+		for x in msg:
+			if x in string_upercase:
+				ind = string_upercase.index(x)
+				ret += string_upercase[ind - shift]
+			if x in string_lowercase:
+				ind = string_lowercase.index(x)
+				ret += string_lowercase[ind - shift]
+			else:
+				ret +=x
+			return ret
